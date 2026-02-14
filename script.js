77a15a0483e590d7026182ad5fb6f7bd09611800
Ritthica S <ritthica@gmail.com>
2026-02-14 21:58:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert(name){
+    alert(name + " details will be available soon!");
+}
+
+function scrollToServices(){
+    document.getElementById("services")
+            .scrollIntoView({behavior:"smooth"});
+}
